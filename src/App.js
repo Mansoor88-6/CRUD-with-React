5b91ae34d6490878fb658a6d32cb965d6cbeb9a0
Mansoor Khan <mkniazi.886@gmail.com>
2023-04-19 22:10:03 +0500

    initial commit,basic CRUD complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Update from './components/Update';
+import Create from './components/Create';
+import Listing from './components/Listing'
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element= {<Home />}/>
+        <Route path='/listing' element={<Listing />}/>
+          <Route path='/create' element={<Create/>} />
+          <Route path='/update/:id' element={<Update/>} />
+          
+          
+        
+      </Routes>
+    
+    </BrowserRouter>
+    
   );
 }
 
